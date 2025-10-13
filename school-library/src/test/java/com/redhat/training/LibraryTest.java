6388185cc62a3fa707ae54dbb781a306ca3a7156
rodrigo.stiven <rodrigo.stiven@gmail.com>
2025-10-13 12:01:03 -0300

    Refactor

--- school-library/src/test/java/com/redhat/training/LibraryTest.java
+++ school-library/src/test/java/com/redhat/training/LibraryTest.java
@@ -23,5 +23,17 @@ public class LibraryTest {
         library = new Library(inventory);
     }
 
-    // Add tests here...
+    @Test
+public void checkingOutDecreasesNumberOfBookCopiesFromInventory()
+    throws BookNotAvailableException {
+    // Given
+    inventory.add(new Book("book1"));
+    inventory.add(new Book("book1"));
+
+    // When
+    library.checkOut("someStudentId", "book1");
+
+    // Then
+    assertEquals(1, inventory.countCopies("book1"));
+}
 }
